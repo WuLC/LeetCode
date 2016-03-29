5c47e25969ce418331a0ca5628c22f028c397345
WuLC <liangchaowu5@gmail.com>
2016-03-29 22:26:17 +0800

    commit 22.Generate Parenteses

--- Algorithm/22. Generate Parentheses.py
+++ Algorithm/22. Generate Parentheses.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        res = []
+        self.helper(n,n,'',res)
+        return res
         
+    def  helper(self,left,right,item,res):
+        if left==0 and right==0:
+            res.append(item)
+            return
+        if left>0:
+            self.helper(left-1,right,item+'(',res)
+        if right>left:
+            self.helper(left,right-1,item+')',res)
+
+
+        
+
+
