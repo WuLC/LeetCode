0a6fd7df9401ac30c07c37fac18499e13b03e2f6
WuLC <liangchaowu5@gmail.com>
2016-04-12 10:17:38 +0800

    Add DP solution for problem 039

--- Algorithm/039. Combination Sum.py
+++ Algorithm/039. Combination Sum.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author: LC
 # @Date:   2016-04-11 15:48:31
-# @Last modified by:   LC
-# @Last Modified time: 2016-04-11 15:49:03
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-04-12 10:16:19
 # @Email: liangchaowu5@gmail.com
 
 
+# solution-1,backtracking
 class Solution(object):
     def combinationSum(self, candidates, target):
         res = []
@@ -21,3 +22,23 @@ class Solution(object):
             if target-nums[i] < 0:
                 return  # backtracking
             self.dfs(nums, target-nums[i], i, path+[nums[i]], res)
+
+# solution-2,DP
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        dp = {}
+        dp[0] = []
+        for i in xrange(1,target+1):
+            dp [i] = []
+            if i in candidates:
+                dp[i].append([i])
+            for j in xrange(1,i/2+1):
+                if len(dp[j]) ==0 or len(dp[i-j])==0:
+                    continue
+                for m in dp[j]:
+                    for k in dp[i-j]:
+                        tmp = m+k
+                        tmp.sort()
+                        if tmp not in dp[i]:
+                            dp[i].append(tmp)
+        return dp[target]
