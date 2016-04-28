0bcf781447bbceb41f95bf996eabcbac7c35af33
WuLC <liangchaowu5@gmail.com>
2016-04-28 09:51:17 +0800

    Add another solution for 048

--- Algorithm/048. Rotate Image.py
+++ Algorithm/048. Rotate Image.py
@@ -2,9 +2,10 @@
 # @Author: LC
 # @Date:   2016-04-27 10:19:13
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-04-27 10:19:27
+# @Last Modified time: 2016-04-28 09:48:20
 # @Email: liangchaowu5@gmail.com
 
+# space complexity O(n)
 class Solution(object):
     def rotate(self, matrix):
         """
@@ -21,4 +22,31 @@ class Solution(object):
             for j in xrange(n):
                 matrix[j][n-1-i] = b[i][j]
         
-        
+# space complexity O(1)
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in xrange(n/2):
+            matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+
+        for i in xrange(n):
+            for j in xrange(i+1,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+"""
+clockwise rotate
+first reverse up to down, then swap the symmetry 
+1 2 3     7 8 9     7 4 1
+4 5 6  => 4 5 6  => 8 5 2
+7 8 9     1 2 3     9 6 3
+
+anticlockwise rotate
+first reverse left to right, then swap the symmetry
+1 2 3     3 2 1     3 6 9
+4 5 6  => 6 5 4  => 2 5 8
+7 8 9     9 8 7     1 4 7
+"""
