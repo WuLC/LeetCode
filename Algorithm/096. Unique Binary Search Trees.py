f9278120f071f0b15f05c7cc77c87f0f45754578
WuLC <liangchaowu5@gmail.com>
2016-06-06 14:03:51 +0800

    Add DP solution

--- Algorithm/096. Unique Binary Search Trees.py
+++ Algorithm/096. Unique Binary Search Trees.py
@@ -2,7 +2,7 @@
 # @Author: LC
 # @Date:   2016-06-05 11:37:16
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-06-05 11:38:07
+# @Last Modified time: 2016-06-06 14:03:29
 # @Email: liangchaowu5@gmail.com
 
 # method 1,divide and conquer
@@ -26,3 +26,15 @@ class Solution(object):
 
 
 # method 2,DP
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for i in xrange(n+1)]
+        dp[0],dp[1] = 1, 1
+        for i in xrange(2,n+1):
+            for j in xrange(i):
+                dp[i] += dp[j]*dp[i-j-1]
+        return dp[n]
