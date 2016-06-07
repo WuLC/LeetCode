9862d73a15786f61e303644548dd507e6a575bce
WuLC <liangchaowu5@gmail.com>
2016-06-07 15:25:47 +0800

    Base on quick sort

---
+++ Algorithm/215. Kth Largest Element in an Array.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Author: WuLC
+# @Date:   2016-06-07 15:24:56
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-06-07 15:25:08
+# @Email: liangchaowu5@gmail.com
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return self.helper(nums, 0, len(nums)-1, k)
+        
+    def helper(self, nums, left, right, k):
+        if left > right:
+            return 
+        i, j = left, right
+        while i < j:
+            while i<j and nums[j] >= nums[left]:
+                j -= 1
+            while i<j and nums[i] <= nums[left]:
+                i += 1
+            if i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i], nums[left] = nums[left], nums[i]
+        if i == len(nums)-k:
+            return nums[i]
+        elif i< len(nums)-k:
+            return self.helper(nums, i+1, right, k)
+        else:
+            return self.helper(nums, left, i-1, k) 
