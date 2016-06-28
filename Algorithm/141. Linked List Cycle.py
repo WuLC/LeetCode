f93eb131d20cc34d5baf60bd13ba9c984bc17b83
WuLC <liangchaowu5@gmail.com>
2016-06-28 14:28:32 +0800

    Two pointers

--- Algorithm/141. Linked List Cycle.py
+++ Algorithm/141. Linked List Cycle.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-06-15 21:31:56
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-06-15 21:40:13
+# @Last Modified time: 2016-06-28 14:28:20
 # @Email: liangchaowu5@gmail.com
 
 
@@ -12,6 +12,7 @@
 #         self.val = x
 #         self.next = None
 
+# method 1, modify the link list
 class Solution(object):
     def hasCycle(self, head):
         """
@@ -23,4 +24,22 @@ class Solution(object):
             p = head.next 
             head.next = head
             head = p
+        return False
+
+# method 2, tow pointers
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        p1 = p2 = head
+        while p1 and p2:
+            if p1.next:
+                p1 = p1.next.next
+            else:
+                return False
+            p2 = p2.next
+            if p1 == p2:
+                return True
         return False
---
+++ Algorithm/142. Linked List Cycle II.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# @Author: WuLC
+# @Date:   2016-06-27 23:09:22
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-06-28 14:22:05
+# @Email: liangchaowu5@gmail.com
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# method 1, hash table, TLE
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        nodes = []
+        while head:
+            if head in nodes:
+                return head
+            nodes.append(head)
+            head = head.next
+        return None
+
+# method 2 ,two pointers
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return None
+        p1, p2 = head, head
+        while p1 and p2:
+            if p1.next:
+                p1 = p1.next.next
+            p2 = p2.next
+            if p1 == p2:
+                p1 = head
+                while p1 and p2:
+                    if p1 == p2:
+                        return p1  
+                    p1 = p1.next
+                    p2 = p2.next
+        return None
