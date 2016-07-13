0f8b4ed47a0b06448fa77039fc202a1de729eea8
WuLC <liangchaowu5@gmail.com>
2016-07-13 21:33:45 +0800

    Find the maximal distance that each step can reach

--- Algorithm/045. Jump Game II.py
+++ Algorithm/045. Jump Game II.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-04-20 09:21:22
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-04-22 10:17:45
+# @Last Modified time: 2016-07-13 21:32:52
 # @Email: liangchaowu5@gmail.com
 
 
@@ -50,4 +50,26 @@ class Solution(object):
                 max_reach = current_reach_max
                 steps+=1
             i+=1
-                
+
+# another solution similar to the above greedy method
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1: 
+            return 0
+        index, steps = 0, 1
+        curr_max_reach, next_max_reach = nums[0], nums[0]
+        while index < len(nums):
+            next_max_reach = max(index+nums[index], next_max_reach)
+            if next_max_reach >= len(nums)-1:
+                if index == 0:
+                    return steps  # the initial place can reach the end in a step
+                else:
+                    return steps + 1
+            if index == curr_max_reach:
+                steps += 1
+                curr_max_reach = next_max_reach 
+            index += 1
