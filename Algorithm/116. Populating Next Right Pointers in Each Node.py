d4e7e70d17a04f25bf3d81d7d1f5aa8d57562ff7
WuLC <liangchaowu5@gmail.com>
2016-07-13 15:57:48 +0800

    Method with constant space

--- Algorithm/116. Populating Next Right Pointers in Each Node.py
+++ Algorithm/116. Populating Next Right Pointers in Each Node.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-07-13 14:56:28
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-07-13 14:56:35
+# @Last Modified time: 2016-07-13 15:21:25
 # @Email: liangchaowu5@gmail.com
 
 # Definition for binary tree with next pointer.
@@ -13,6 +13,7 @@
 #         self.right = None
 #         self.next = None
 
+# method 1, O(n) space , n defines the maximal number of nodes of  levels
 class Solution(object):
     def connect(self, root):
         """
@@ -32,3 +33,19 @@ class Solution(object):
                 if i != len(nodes)-1:
                     nodes[i].next = nodes[i+1]
             nodes = next_level
+
+# method 2, O(1) space
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        while root:
+           cur = root
+           while cur and cur.left:
+               cur.left.next = cur.right
+               if cur.next:
+                   cur.right.next = cur.next.left
+               cur = cur.next
+           root = root.left
