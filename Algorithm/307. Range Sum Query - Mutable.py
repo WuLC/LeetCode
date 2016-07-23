f42d19b96be6715c48cd9fcef4b33db0248d262a
WuLC <liangchaowu5@gmail.com>
2016-07-23 21:43:46 +0800

    Add one more element to BIT, thus there is no need to take 0 as an exception

--- Algorithm/307. Range Sum Query - Mutable.py
+++ Algorithm/307. Range Sum Query - Mutable.py
@@ -2,9 +2,10 @@
 # @Author: WuLC
 # @Date:   2016-07-23 14:37:57
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-07-23 14:38:33
+# @Last Modified time: 2016-07-23 21:42:58
 # @Email: liangchaowu5@gmail.com
 
+# method 1
 class NumArray(object):
     def __init__(self, nums):
         """
@@ -62,3 +63,50 @@ class NumArray(object):
     def lowest_bit(self, num):
         return num&(-num)
         
+
+# method 2,make the above code more concise
+# the above code take 0 as an exception, the following generalize it by adding one more element to count
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.nums = nums[:]
+        self.count = [0 for i in xrange(len(nums)+1)]
+        for i in xrange(len(nums)):
+            self.initialize(i, nums[i])
+    
+    def initialize(self, i, val):
+        i += 1
+        while i < len(self.nums)+1:
+            self.count[i] += val
+            i += (i & -i)
+        
+        
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        diff = val - self.nums[i]
+        self.nums[i] = val
+        self.initialize(i, diff)
+    
+    def left_sum(self, i):
+        i += 1
+        total = 0
+        while i>0:
+            total += self.count[i]
+            i -= (i & -i)
+        return total
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.left_sum(j) - self.left_sum(i-1)
