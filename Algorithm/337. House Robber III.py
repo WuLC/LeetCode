5a00f6384782a10de3f2cc9d9c0db96801a67f0e
WuLC <liangchaowu5@gmail.com>
2016-07-30 20:38:55 +0800

    DFS,divide and conquer

---
+++ Algorithm/337. House Robber III.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author: WuLC
+# @Date:   2016-07-30 20:37:33
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-07-30 20:37:40
+# @Email: liangchaowu5@gmail.com
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return max(self.dfs(root))
+    
+    def dfs(self, root):
+        if root == None:
+            return (0,0)
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        return (max(left[1],left[0])+max(right[0],right[1]), root.val+left[0]+right[0])
