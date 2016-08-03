7e7ea801ffdabbe1c18eb41d270c392438030901
WuLC <liangchaowu5@gmail.com>
2016-08-03 17:23:43 +0800

    Binary search, two methods

--- Algorithm/154. Find Minimum in Rotated Sorted Array II.py
+++ Algorithm/154. Find Minimum in Rotated Sorted Array II.py
@@ -2,9 +2,10 @@
 # @Author: WuLC
 # @Date:   2016-08-03 17:16:31
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-08-03 17:17:19
+# @Last Modified time: 2016-08-03 17:23:24
 # @Email: liangchaowu5@gmail.com
 
+# method 1
 class Solution(object):
     def findMin(self, nums):
         """
@@ -28,4 +29,22 @@ class Solution(object):
                 else:
                     right = mid
         return nums[left]
-        
+ 
+# method 2
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0: return None
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left+right)/2
+            if nums[right] < nums[mid]:
+                left = mid + 1
+            elif nums[right] > nums[mid]:
+                right = mid
+            else:
+                right -= 1
+        return nums[left]       
