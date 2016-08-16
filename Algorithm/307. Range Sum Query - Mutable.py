ded47d934cc217ac7cb9babf110f03964eb8ce18
WuLC <liangchaowu5@gmail.com>
2016-08-16 22:28:24 +0800

    Add solution with segment tree

--- Algorithm/307. Range Sum Query - Mutable.py
+++ Algorithm/307. Range Sum Query - Mutable.py
@@ -2,10 +2,10 @@
 # @Author: WuLC
 # @Date:   2016-07-23 14:37:57
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-07-23 21:42:58
+# @Last Modified time: 2016-08-16 22:26:41
 # @Email: liangchaowu5@gmail.com
 
-# method 1
+# method 1, binary indexed tree
 class NumArray(object):
     def __init__(self, nums):
         """
@@ -110,3 +110,64 @@ class NumArray(object):
         :rtype: int
         """
         return self.left_sum(j) - self.left_sum(i-1)
+
+
+# method 3, segment tree
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        n = len(nums)
+        if n == 0: return
+        max_size = 2 * pow(2, int(math.ceil(math.log(n, 2)))) - 1
+        self.seg_tree = [0 for i in xrange(max_size)]
+        self.nums = nums[:]
+        self.build_tree(0, n-1, 0)
+        
+    def build_tree(self, start, end, curr):
+        if start > end: return # empty list
+        if start == end:
+            self.seg_tree[curr] = self.nums[start]
+        else:
+            mid = start + (end - start)/2
+            self.seg_tree[curr] = self.build_tree(start, mid, curr*2+1) + self.build_tree(mid+1, end, curr*2+2)
+        return self.seg_tree[curr]        
+        
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        diff = val - self.nums[i]
+        self.nums[i] = val
+        self.update_sum(0, len(self.nums)-1, i, 0, diff)
+    
+    def update_sum(self, start, end, idx, curr, diff):
+        self.seg_tree[curr] += diff
+        if start == end: return
+        mid = start + (end - start)/2
+        if start <= idx <= mid:
+            self.update_sum(start, mid, idx, curr*2+1, diff)
+        else:
+            self.update_sum(mid+1, end, idx, curr*2+2, diff)
+        
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.get_sum(0, len(self.nums)-1, i, j, 0)
+        
+    def get_sum(self, start, end, qstart, qend, curr):
+        mid = start + (end - start)/2
+        if qstart > end or qend < start:
+            return 0
+        elif start >= qstart and end <= qend:
+            return self.seg_tree[curr]
+        else:
+            return self.get_sum(start, mid, qstart, qend, curr*2+1) + self.get_sum(mid+1, end, qstart, qend, curr*2+2)
