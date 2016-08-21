4ab9a368ce1621be48f3af29906433c6f0d73c2c
WuLC <liangchaowu5@gmail.com>
2016-08-21 14:33:33 +0800

    The first one who got the number that is multiple of 4 (i.e. n % 4== 0) will lost, otherwise he/she will win

--- Algorithm/292. Nim Game.py
+++ Algorithm/292. Nim Game.py
@@ -2,9 +2,10 @@
 # @Author: WuLC
 # @Date:   2016-08-21 13:22:00
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-08-21 13:22:08
+# @Last Modified time: 2016-08-21 14:33:19
 # @Email: liangchaowu5@gmail.com
 
+# The first one who got the number that is multiple of 4 (i.e. n % 4== 0) will lost, otherwise he/she will win
 class Solution(object):
     def canWinNim(self, n):
         """
