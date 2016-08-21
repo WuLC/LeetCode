8680fa7f3b1b44de7f5af5d4bd685fa6d456511f
WuLC <liangchaowu5@gmail.com>
2016-08-21 08:03:28 +0800

    DP and binary search， O(n*logn)

--- Algorithm/300. Longest Increasing Subsequence.py
+++ Algorithm/300. Longest Increasing Subsequence.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-08-19 17:56:52
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-08-19 21:11:59
+# @Last Modified time: 2016-08-21 08:03:13
 # @Email: liangchaowu5@gmail.com
 
 # method 1,dynamic programming , O(n^2)
@@ -22,4 +22,31 @@ class Solution(object):
             result = max(result, dp[i])
         return result
 
-# method 2, 
+# method 2, DP and binary search， O(n*logn)
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = []
+        for num in nums:
+            if len(dp) == 0 or len(dp) > 0 and num > dp[-1]:
+                dp.append(num)
+            else:
+                self.binary_search(dp, num)
+        return len(dp)
+    
+    
+    def binary_search(self, dp, num):
+        left, right = 0, len(dp)-1
+        while left < right:
+            mid = left + (right - left)/2
+            if dp[mid] == num:
+                return
+            elif dp[mid] > num:
+                right = mid 
+            else:
+                left = mid + 1
+        if dp[left] > num:
+            dp[left] = num
