17c54639c68b68137c49df559a3ffd8347f4ea95
WuLC <liangchaowu5@gmail.com>
2016-08-22 09:17:29 +0800

    DP,pay attention that some numbers are larger than the maximum product after seperating

---
+++ Algorithm/343. Integer Break.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: WuLC
+# @Date:   2016-08-22 09:13:55
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-08-22 09:14:28
+# @Email: liangchaowu5@gmail.com
+
+# Dynamic Programming
+# pay attention that this 
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for i in xrange(n+1)]
+        dp[1] = 1
+        for i in xrange(2,n+1):
+            for j in xrange(1, i/2+1):
+                dp[i] = max(dp[i], max(dp[j],j)*max(dp[i-j],i-j))
+        return dp[n]
+            
+        
