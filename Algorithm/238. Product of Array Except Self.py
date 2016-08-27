ef1190ccf928a1fcc8b141d3f0fcf9d702aeb8c6
WuLC <liangchaowu5@gmail.com>
2016-08-27 20:40:18 +0800

    O(n) time, O(1) space without division

--- Algorithm/238. Product of Array Except Self.py
+++ Algorithm/238. Product of Array Except Self.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-08-27 20:24:48
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-08-27 20:26:14
+# @Last Modified time: 2016-08-27 20:40:07
 # @Email: liangchaowu5@gmail.com
 
 # O(n) time, O(n) space without division
@@ -29,5 +29,20 @@ class Solution(object):
             else:
                 result.append(left[i-1]*right[n-i-2])
         return result
+ 
                 
-            
+# O(n) time, O(1) space without division
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result, n = [1], len(nums)
+        for i in xrange(1, n):
+            result.append(result[i-1]*nums[i-1])
+        right = 1
+        for i in reversed(xrange(n)):
+            result[i] *=right
+            right *= nums[i]
+        return result    
