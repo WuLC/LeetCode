8dcad0bc519d9144ef98c3f874cd0bb73720bc50
WuLC <liangchaowu5@gmail.com>
2016-08-28 19:57:40 +0800

    Sort and DP, O(n^^2)

--- Algorithm/368. Largest Divisible Subset.py
+++ Algorithm/368. Largest Divisible Subset.py
@@ -2,11 +2,11 @@
 # @Author: WuLC
 # @Date:   2016-06-27 21:34:45
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-06-28 20:26:01
+# @Last Modified time: 2016-08-28 19:57:18
 # @Email: liangchaowu5@gmail.com
 
 
-# method 1,TLE
+# method 1, judge diectly, O(n^3),TLE
 class Solution(object):
     def largestDivisibleSubset(self, nums):
         """
@@ -31,4 +31,24 @@ class Solution(object):
                 return False
         return True
 
-                
+# method 2,DP, O(n^2), AC
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        dp = [1]*len(nums)
+        record, index = [], 0
+        for i in xrange(len(nums)):
+            tmp = [nums[i]]
+            for j in xrange(i):
+                if nums[i] % nums[j] == 0:
+                    if dp[j]+1 > dp[i]:
+                        tmp = record[j] + [nums[i]]
+                        dp[i] = dp[j] + 1
+            if dp[i] > dp[index]:
+                index = i
+            record.append(tmp[:])
+        return record[index] if record else record              
