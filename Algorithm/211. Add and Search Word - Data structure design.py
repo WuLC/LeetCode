00472d0347f754d2cfb38c287c5b9625e477721c
WuLC <liangchaowu5@gmail.com>
2016-09-26 22:52:36 +0800

    Use Trie without construct Trie class

--- Algorithm/211. Add and Search Word - Data structure design.py
+++ Algorithm/211. Add and Search Word - Data structure design.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-06-30 22:57:00
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-09-26 18:09:18
+# @Last Modified time: 2016-09-26 22:50:27
 # @Email: liangchaowu5@gmail.com
 
 
@@ -60,17 +60,9 @@ class TrieNode:
 
 class WordDictionary(object):
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.root = TrieNode()
 
     def addWord(self, word):
-        """
-        Adds a word into the data structure.
-        :type word: str
-        :rtype: void
-        """
         curr = self.root
         for char in word:
             if char not in curr.children:
@@ -80,12 +72,6 @@ class WordDictionary(object):
                 
 
     def search(self, word):
-        """
-        Returns if the word is in the data structure. A word could
-        contain the dot character '.' to represent any one letter.
-        :type word: str
-        :rtype: bool
-        """
         def helper(root, word):
             if len(word) == 0  and root.word == True:
                 return True
@@ -100,7 +86,7 @@ class WordDictionary(object):
                 return any([helper(node, word[i+1:]) for node in next])
         return helper(self.root, word)
 
-# change helper   
+# change helper  function 
 # manner 1, beat 23%
 def helper(root, word):
     if len(word) == 0:
@@ -120,3 +106,27 @@ def helper(root, word):
     if word[0] != '.':
         return word[0] in root.children and helper(root.children[word[0]], word[1:])
     return any([helper(node, word[1:]) for node in root.children.values()])
+
+
+# method 3, same idea as method 2
+# but need not to implement the Trie class
+class WordDictionary(object):
+    def __init__(self):
+        self.root = {}
+        
+
+    def addWord(self, word):
+        curr = self.root
+        for char in word:
+            curr.setdefault(char, {})
+            curr = curr[char]
+        curr['#'] = '#'  # specify it as a word
+            
+    def search(self, word):
+        def find(root, word):
+            if len(word) == 0:
+                return '#' in root
+            if word[0] != '.':
+                return word[0] in root and find(root[word[0]], word[1:])
+            return any([find(node, word[1:]) for node in root.values() if node != '#']) # pay attention a word has no children 
+        return find(self.root, word)
