d6274228082091372699c80c2f2e67a6dc698438
WuLC <liangchaowu5@gmail.com>
2016-10-10 18:12:45 +0800

    Add the method that add the two numbers directly, which will overflow in some cases

--- Algorithm/Java/002. Add Two Numbers.java
+++ Algorithm/Java/002. Add Two Numbers.java
@@ -2,7 +2,7 @@
 * Author: LC
 * Date:   2016-10-10 17:09:41
 * Last modified by:   WuLC
-* Last Modified time: 2016-10-10 18:04:39
+* Last Modified time: 2016-10-10 18:11:43
 * Email: liangchaowu5@gmail.com
 */
 
@@ -18,7 +18,7 @@
 
 
 // operate directly on the linked list
-// notice that, get the number of a single linked list will overflow somttimes
+// notice that  get the number of a single linked list will overflow in some cases
 public class Solution 
 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
@@ -58,3 +58,31 @@ public class Solution
 }
 
 
+// add two number directly, overflow in some test cases
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        String num1="", num2 = "";
+        while (l1!=null)
+        {
+            num1 += String.valueOf(l1.val);
+            l1 = l1.next;
+        }
+        while (l2!=null)
+        {
+            num2 += String.valueOf(l2.val);
+            l2 = l2.next;
+        }
+        long sum = Long.parseLong(new StringBuilder(num1).reverse().toString()) + 
+                  Long.parseLong(new StringBuilder(num2).reverse().toString());
+        String str = String.valueOf(sum);
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for (int i = str.length()-1; i>=0; i--)
+        {
+            curr.next = new ListNode(str.charAt(i) - '0');
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
