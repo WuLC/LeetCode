f6371b10bd4f3093bf7bee99eb2ca9218631d0a2
WuLC <liangchaowu5@gmail.com>
2016-10-14 15:55:02 +0800

    Bit manipulation, use bits to detect conflict and lowbit to locate possible positions

--- Algorithm/Java/051. N-Queens.java
+++ Algorithm/Java/051. N-Queens.java
@@ -2,7 +2,7 @@
 * Author: WuLC
 * Date:   2016-10-13 07:59:15
 * Last modified by:   WuLC
-* Last Modified time: 2016-10-13 13:33:49
+* Last Modified time: 2016-10-14 15:53:45
 * Email: liangchaowu5@gmail.com
 */
 
@@ -108,6 +108,122 @@ public class Solution
                     dfs(row + 1, n);
                     column[col] = 0; left_cross[row+col]=0; right_cross[n-1-row+col] = 0;
                 }
+        }
+    }
+}
+
+// method 3
+// same idea as method 2, but use the bits of a long number to replace the arrays in method 2(Bit manipulation)
+// thus there is a max limit to n, that is 32,cause 64 > (2*32 - 1)
+public class Solution 
+{
+    public long column;
+    public long left_cross;
+    public long right_cross;
+    public int[] positions;
+    public List<List<String>> result;
+      
+    public List<List<String>> solveNQueens(int n) 
+    {
+        column = 0;
+        left_cross = 0;
+        right_cross = 0;
+        positions = new int[n];
+        result = new ArrayList<List<String>>();
+        dfs(0, n);
+        return result;
+    }
+    
+    public void dfs(int row, int n)
+    {
+        if (row == n)
+        {
+            List<String> tmp = new ArrayList<String>();
+            char[] pos = new char[n];
+            Arrays.fill(pos, '.');
+            for (int i = 0; i < n; i++)
+            {
+                pos[positions[i]] = 'Q';
+                tmp.add(new String(pos));
+                pos[positions[i]] = '.';
+            }
+            result.add(tmp);
+            return ;
+        }
+        long one = 1;
+        for (int col=0; col < n; col ++)
+        {   
+            if ( (column&(one<<col))!=0 || (left_cross&(one<<(row+col)))!= 0 || (right_cross&(one<<(n-1-row+col)))!=0)
+                continue;
+            else
+                {
+                    column ^= (one<<col); left_cross ^= (one<<(row+col)); right_cross ^= (one<<(n-1-row+col));
+                    positions[row] = col;
+                    dfs(row + 1, n);
+                    column ^= (one<<col); left_cross ^= (one<<(row+col)); right_cross ^= (one<<(n-1-row+col));
+                }
+        }
+    }
+}
+
+
+// method 4
+// with bit manipulation, use lowbit to locate directly which positions cause no conflict
+// there is still the maximal limit for n (n<=32) since there is only 64 bits for the long type
+public class Solution 
+{
+    public long column;
+    public long left_cross;
+    public long right_cross;
+    public int[] positions;
+    public List<List<String>> result;
+    
+    public List<List<String>> solveNQueens(int n) 
+    {
+        column = 0;
+        left_cross = 0;
+        right_cross = 0;
+        positions = new int[n];
+        result = new ArrayList<List<String>>();
+        dfs(0, n);
+        return result;
+    }
+    
+    public void dfs(int row, int n)
+    {
+        if (row == n)
+        {
+            List<String> tmp = new ArrayList<String>();
+            char[] pos = new char[n];
+            Arrays.fill(pos, '.');
+            for (int i = 0; i < n; i++)
+            {
+                pos[positions[i]] = 'Q';
+                tmp.add(new String(pos));
+                pos[positions[i]] = '.';
+            }
+            result.add(tmp);
+            return ;
+        }
+        long one = 1;
+        // find the positions of columns for the current row, make the possible positions 1 and impossible positions 0
+        long available =  ((one<<n)-1) & ~(column | (left_cross>>row) | (right_cross >> (n-1-row)));
+        long lowBit, tmp;
+        int count;
+        while (available != 0)
+        {
+            count = 0;
+            lowBit = tmp =  available & -available;
+            available ^= lowBit;
+            while (tmp > 0)
+            {
+                tmp >>= 1;
+                count += 1;
+            } 
+            positions[row] = count-1;
+            column ^= lowBit; left_cross ^= (lowBit<<row); right_cross ^= (lowBit<<(n-1-row));
+            dfs(row+1, n);
+            column ^= lowBit; left_cross ^= (lowBit<<row); right_cross ^= (lowBit<<(n-1-row));
         }
     }
 }
