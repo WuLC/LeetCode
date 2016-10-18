c2e16f1aaecca58fb3209a42b13228a056b948ed
WuLC <liangchaowu5@gmail.com>
2016-10-18 17:45:39 +0800

    Turn the number to string or take and reverse the lower half of the number

---
+++ Algorithm/Java/009. Palindrome Number.java
@@ -0,0 +1,42 @@
+/**
+* Author: LC
+* Date:   2016-10-18 17:36:30
+* Last modified by:   WuLC
+* Last Modified time: 2016-10-18 17:44:28
+* Email: liangchaowu5@gmail.com
+*/
+
+// method 1
+// turn int to string and then judge
+public class Solution 
+{
+    public boolean isPalindrome(int x) 
+    {
+        String s = String.valueOf(x);
+        int i = 0, j = s.length() - 1;
+        while (i < j)
+        {
+            if (s.charAt(i++) != s.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+}
+
+
+// method 2
+// operate on the number, just take and reverse the low half of the number
+public class Solution 
+{
+    public boolean isPalindrome(int x) 
+    {
+        if (x < 0 || x > 0 && x%10 == 0) return false;
+        int tmp = 0;
+        while (x > tmp)
+        {
+            tmp = tmp * 10 + x%10;
+            x /= 10;
+        }
+        return (x == tmp || x == tmp/10);
+    }
+}
