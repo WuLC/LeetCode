d92fe1c4a20a3c02d29886dc794ca8fa1a11fa68
WuLC <liangchaowu5@gmail.com>
2016-10-21 00:16:38 +0800

    Transfer the problem to finding the kth element of two sorted arrays, time complexity logk

--- Algorithm/Java/004. Median of Two Sorted Arrays.java
+++ Algorithm/Java/004. Median of Two Sorted Arrays.java
@@ -2,12 +2,12 @@
 * Author: WuLC
 * Date:   2016-10-17 22:45:00
 * Last modified by:   WuLC
-* Last Modified time: 2016-10-17 23:55:03
+* Last Modified time: 2016-10-21 00:16:01
 * Email: liangchaowu5@gmail.com
 */
 
-
-// 
+// transfer the problem to finding the kth element of two sorted arrays, which should be the first k/2+1 numbers of both array
+// time complexity(logk)    
 public class Solution 
 {
     private int nums1Left;
@@ -32,7 +32,7 @@ public class Solution
             return (smaller + bigger)/2.0;
         }
     }
-    
+
     /*find the kth element of two sorted arrays*/
     public int findKthNumber(int[] nums1, int[] nums2, int k)
     {
@@ -44,13 +44,22 @@ public class Solution
             return Math.min(nums1[nums1Left], nums2[nums2Left]);
             
         int n1=0, n2=0;
-        if (nums1Right - nums1Left + 1 >= k/2)  n1 = nums1[nums1Left + k/2 - 1];
-        if (nums2Right - nums2Left + 1 >= k/2)  n2 = nums2[nums2Left + k/2 - 1];
+        int nums1Length = nums1Right - nums1Left + 1, nums2Length = nums2Right - nums2Left + 1;
+        if (nums1Length >= k/2)  n1 = nums1[nums1Left + k/2 - 1];
+        if (nums2Length >= k/2)  n2 = nums2[nums2Left + k/2 - 1];
         
-        if (nums1Right - nums1Left + 1  < k/2) nums2Left += k/2;
-        else if (nums2Right - nums2Left + 1 < k/2)  nums1Left += k/2;
-        else if (n1 < n2) nums1Left += k/2; 
-        else nums2Left += k/2; 
+        if (nums1Length  < k/2) nums2Left += k/2;
+        else if (nums2Length < k/2)  nums1Left += k/2;
+        else if (n1 > n2) 
+        {
+            nums2Left += k/2;  // remove the 
+            if (nums1Left + k/2 <= nums1Right) nums1Right = nums1Left + k/2;
+        }
+        else 
+        {
+            nums1Left += k/2;
+            if (nums2Left + k/2 <= nums2Right) nums2Right = nums2Left + k/2;
+        }
         return findKthNumber(nums1, nums2, k - k/2);
     }
 }
