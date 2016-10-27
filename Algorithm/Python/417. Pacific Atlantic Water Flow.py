d85704ec8193471bad6a0549153a0ac1638bd496
WuLC <liangchaowu5@gmail.com>
2016-10-27 22:22:19 +0800

    DFS, use two visit matrix, one for pacific and the other for atlantic, from edge to begin dfs

--- Algorithm/Python/417. Pacific Atlantic Water Flow.py
+++ Algorithm/Python/417. Pacific Atlantic Water Flow.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2016-10-25 21:24:59
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-10-25 22:14:30
+# @Last Modified time: 2016-10-27 22:21:50
 # @Email: liangchaowu5@gmail.com
 
 
@@ -71,3 +71,45 @@ class Solution(object):
         self.visited[i][j] = 0
         
     
+
+
+# DFS
+# referer: https://discuss.leetcode.com/topic/62379/java-bfs-dfs-from-ocean
+# use two visit matrix, one for pacific and the other for atlantic
+# for either ocean, from the edge of ocean to reach any positioin that it can reach and update the visited table
+# finally, the positions that can be visited in both visite table is the positions that can visit both ocean
+
+class Solution(object):
+    moves = ((0,1), (0,-1), (1,0), (-1,0))
+    def pacificAtlantic(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        result = []
+        m = len(matrix)
+        if m == 0:
+            return result
+        n = len(matrix[0])
+        p_visited = [[0 for j in xrange(n)] for i in xrange(m)]
+        a_visited = [[0 for j in xrange(n)] for i in xrange(m)]
+        for i in xrange(m):
+            self.dfs(i, 0, m, n, matrix, p_visited)
+            self.dfs(i, n-1, m, n, matrix, a_visited)
+        for j in xrange(n):
+            self.dfs(0, j, m, n, matrix, p_visited)
+            self.dfs(m-1, j, m, n, matrix, a_visited)
+             
+        for i in xrange(m):
+            for j in xrange(n):
+                if p_visited[i][j] == 1 and a_visited[i][j] == 1:
+                    result.append([i, j])
+        return result
+        
+    def dfs(self, i, j, m, n, matrix, visited):
+        visited[i][j] = 1
+        for move in Solution.moves:
+            x, y = i+move[0], j+move[1]
+            if 0<=x<m and 0<=y<n and visited[x][y] == 0 and matrix[i][j] <= matrix[x][y]:
+                self.dfs(x, y, m, n, matrix, visited)
+        
