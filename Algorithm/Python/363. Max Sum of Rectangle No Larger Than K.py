4b1c02ad48507042b85701b66610e71daa6d9fd3
WuLC <liangchaowu5@gmail.com>
2016-11-21 15:45:04 +0800

    Base on max subarray no larger than k in 1-D array and max sum of rectangle in an 2-D matrix, with bisect make it faster

---
+++ Algorithm/Python/363. Max Sum of Rectangle No Larger Than K.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# @Author: LC
+# @Date:   2016-11-21 14:55:26
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-11-21 15:44:45
+# @Email: liangchaowu5@gmail.com
+
+# base on max subarray no larger than k in 1-D array and max sum of rectangle in an 2-D matrix
+# referer:
+# https://discuss.leetcode.com/topic/48875/accepted-c-codes-with-explanation-and-references
+# https://www.youtube.com/watch?v=yCQN096CwWM
+# https://www.quora.com/Given-an-array-of-integers-A-and-an-integer-k-find-a-subarray-that-contains-the-largest-sum-subject-to-a-constraint-that-the-sum-is-less-than-k
+
+
+# brute force, TLE
+# time complexity O((row*col)^2)
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        m, n = len(matrix), len(matrix[0])
+        result = None
+        for left in xrange(n):
+            sums = [0 for i in xrange(m)]
+            for right in xrange(left, n):
+                for i in xrange(m):
+                    sums[i] += matrix[i][right]
+                cumulative_sum, curr_sum = [0], 0
+                for i in xrange(m):
+                    curr_sum += sums[i]
+                    for cs in cumulative_sum:
+                        if curr_sum-cs <= k  and (result == None or result < curr_sum-cs):
+                            result = curr_sum - cs
+                    cumulative_sum.append(curr_sum)
+        return result
+                        
+                
+# same idea as above, but with bitsect makes it faster
+# AC(1276ms), but theoretically its time complexity is the same as the above solution
+import bisect
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        m, n = len(matrix), len(matrix[0])
+        result = -pow(2,31)
+        for left in xrange(n):
+            sums = [0 for i in xrange(m)]
+            for right in xrange(left, n):
+                for i in xrange(m):
+                    sums[i] += matrix[i][right]
+                cumulative_sum, curr_sum = [0], 0
+                for i in xrange(m):
+                    curr_sum += sums[i]
+                    index = bisect.bisect_left(cumulative_sum, curr_sum - k)
+                    if index != len(cumulative_sum):
+                        result = max(result, curr_sum - cumulative_sum[index])
+                    bisect.insort(cumulative_sum, curr_sum)
+        return result
