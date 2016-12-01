b4f98a48f77b34dda4c834092d754652386575fe
WuLC <liangchaowu5@gmail.com>
2016-12-01 21:26:02 +0800

    Stack, O(n) time, traverse backwards

--- Algorithm/Python/456. 132 Pattern.py
+++ Algorithm/Python/456. 132 Pattern.py
@@ -2,7 +2,7 @@
 # @Author: LC
 # @Date:   2016-11-30 11:44:28
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-11-30 11:46:09
+# @Last Modified time: 2016-12-01 21:25:36
 # @Email: liangchaowu5@gmail.com
 
 # naivve solution, O(n^2), TLE
@@ -24,4 +24,21 @@ class Solution(object):
                 curr_min = nums[i]
         return False
 
-# 
+
+# stack, O(n) time, traverse backwards 
+# maintain s2 > s3 and keep s3 as large as possible
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        stack = []
+        s3 = None
+        for i in reversed(xrange(len(nums))):
+            if s3 and s3 > nums[i]:
+                return True
+            while stack and nums[i] > stack[-1]:
+                s3 = stack.pop()
+            stack.append(nums[i])
+        return False
