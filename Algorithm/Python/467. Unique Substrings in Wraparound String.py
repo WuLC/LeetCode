a8ec3447f17d3feabf2d5b1f82f4f9b3d06505cf
WuLC <liangchaowu5@gmail.com>
2016-12-06 15:20:54 +0800

    Time O(n), find the longest continuous substrings that end with different characters in p

---
+++ Algorithm/Python/467. Unique Substrings in Wraparound String.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: LC
+# @Date:   2016-12-06 15:12:56
+# @Last modified by:   WuLC
+# @Last Modified time: 2016-12-06 15:19:30
+# @Email: liangchaowu5@gmail.com
+
+# time complexity: O(n)
+# find the longest continous substrings that end with defferent characters in p
+# thus the length of the longest continous substring that ends with certain character 
+# is the number of the substrings in p that ends with certain character 
+# for example: 'abcbc', the longest continous substring the ends with 'c' is 'abc', thus the number of substrings that in s is 3(abc,bc,c)
+class Solution(object):
+    def findSubstringInWraproundString(self, p):
+        """
+        :type p: str
+        :rtype: int
+        """
+        count = [0 for i in xrange(26)]
+        left, right = 0, 0
+        while right < len(p):
+            idx = ord(p[right])-97
+            if right-1 >=0 and ord(p[right]) - ord(p[right-1]) not in {1, -25}:
+                left = right
+            count[idx] = max(count[idx], right-left+1) 
+            right += 1
+        return sum(count)
