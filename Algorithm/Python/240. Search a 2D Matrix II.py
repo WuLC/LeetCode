5d717e5f6256e6e2b78cd80105ef46d2260018e2
WuLC <liangchaowu5@gmail.com>
2016-12-29 23:17:49 +0800

    Compare the number on the  top right corner with target, if equal, return True,else delete a row or column each time

--- Algorithm/Python/240. Search a 2D Matrix II.py
+++ Algorithm/Python/240. Search a 2D Matrix II.py
@@ -2,9 +2,10 @@
 # @Author: WuLC
 # @Date:   2016-07-31 10:34:18
 # @Last modified by:   WuLC
-# @Last Modified time: 2016-07-31 10:34:25
+# @Last Modified time: 2016-12-29 23:17:15
 # @Email: liangchaowu5@gmail.com
 
+# method 1,binary search
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -34,4 +35,29 @@ class Solution(object):
                 left = mid + 1
         if left == right:
             return matrix[row][left] == target
+
+
+# method 2, faster than method 1
+# compare the number on the  top right corner with target 
+# if equal, return True
+# else delete a row or column each time 
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        row, col = 0, n-1
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+        return False
         
