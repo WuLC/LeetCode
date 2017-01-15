c979524312513f2b5363b53c49704e0ed7252193
WuLC <liangchaowu5@gmail.com>
2017-01-15 17:14:00 +0800

    Compare target with the number on top right corner, and decide whether to delete the row or the column

---
+++ Algorithm/Java/240. Search a 2D Matrix II.java
@@ -0,0 +1,26 @@
+/**
+* Author: WuLC
+* Date:   2017-01-15 17:13:29
+* Last modified by:   WuLC
+* Last Modified time: 2017-01-15 17:13:50
+* Email: liangchaowu5@gmail.com
+*/
+
+// compare target with the number on top right corner, and decide whether to delete the row or the column
+public class Solution 
+{
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+        int m = matrix.length;
+        if (m==0) return false;
+        int n = matrix[0].length;
+        int row = 0,  col = n-1;
+        while (row < m && col >= 0)
+        {
+            if (matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
+}
