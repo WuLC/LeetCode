e20b8345ede2eec1ee2242d573729d249b389d29
WuLC <liangchaowu5@gmail.com>
2017-02-05 01:41:46 +0800

    Dp, AC

--- Algorithm/Python/494. Target Sum.py
+++ Algorithm/Python/494. Target Sum.py
@@ -2,11 +2,11 @@
 # @Author: WuLC
 # @Date:   2017-02-02 21:34:27
 # @Last modified by:   WuLC
-# @Last Modified time: 2017-02-04 23:59:01
+# @Last Modified time: 2017-02-05 01:41:24
 # @Email: liangchaowu5@gmail.com
 
 
-# naive dfs, TLE
+# naive dfs, O(2^n), TLE
 class Solution(object):
     def findTargetSumWays(self, nums, S):
         """
@@ -27,6 +27,7 @@ class Solution(object):
             for flag in [1,-1]:
                 self.dfs(idx+1, count+flag*nums[idx], S, nums)
 
+
 # naive recursive, TLE
 class Solution(object):
     def findTargetSumWays(self, nums, S):
@@ -41,3 +42,20 @@ class Solution(object):
         return self.findTargetSumWays(nums[:], S - curr_num) + self.findTargetSumWays(nums[:], S + curr_num)
         
 
+# dp, AC
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        count = collections.defaultdict(int)
+        count[0] = 1
+        for num in nums:
+            tmp = collections.defaultdict(int)
+            for k,v in count.items():
+                tmp[k + num] += v
+                tmp[k - num] += v
+            count = tmp
+        return count[S]
