5cf5d535f8e2bcdf4e4757c3a49b1c9400701660
WuLC <liangchaowu5@gmail.com>
2017-02-08 20:51:07 +0800

    Stack and hashmap, O(n) time, O(n) space, AC

--- Algorithm/Python/496. Next Greater Element I.py
+++ Algorithm/Python/496. Next Greater Element I.py
@@ -2,10 +2,10 @@
 # @Author: WuLC
 # @Date:   2017-02-08 20:27:23
 # @Last modified by:   WuLC
-# @Last Modified time: 2017-02-08 20:37:24
+# @Last Modified time: 2017-02-08 20:50:59
 # @Email: liangchaowu5@gmail.com
 
-# naive solution, O(mn), AC
+# naive solution, O(mn) time, O(1) space, AC
 class Solution(object):
     def nextGreaterElement(self, findNums, nums):
         """
@@ -25,4 +25,26 @@ class Solution(object):
                     break
             result.append(tmp)
         return result
-                    
+
+
+# stack and hashmap, O(n) time, O(n) space, AC
+class Solution(object):
+    def nextGreaterElement(self, findNums, nums):
+        """
+        :type findNums: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        stack = []
+        numsMap = {}
+        for num in nums:
+            while stack and stack[-1] < num:
+                numsMap[stack.pop()] = num
+            stack.append(num)
+        for num in findNums:
+            if num in numsMap:
+                result.append(numsMap[num])
+            else:
+                result.append(-1)
+        return result
