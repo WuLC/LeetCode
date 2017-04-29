3c5febfed2d18683967294966944be0d193cfe2a
WuLC <liangchaowu5@gmail.com>
2017-04-29 11:53:46 +0800

    Another kind of dp, O(nlogn) time, O(n) space

--- Algorithm/Java/300. Longest Increasing Subsequence.java
+++ Algorithm/Java/300. Longest Increasing Subsequence.java
@@ -2,12 +2,13 @@
 * Author: WuLC
 * Date:   2017-04-29 11:10:15
 * Last modified by:   WuLC
-* Last Modified time: 2017-04-29 11:10:44
+* Last Modified time: 2017-04-29 11:53:15
 * Email: liangchaowu5@gmail.com
 */
 
 
-// dp, O(n^2) time, O(n) space
+// dp, dp[i] represents the length of longest subsequence that ends with nums[i]
+// O(n^2) time, O(n) space
 public class Solution 
 {
     public int lengthOfLIS(int[] nums) 
@@ -23,5 +24,38 @@ public class Solution
             result = Math.max(result, dp[i]);
         }
         return result;
+    }
+}
+
+// dp, dp[i] represents the smallest number among those numbers 
+// that act as the last number of the increasing subsequence of length i+1
+// O(nlogn) time, O(n) space
+public class Solution 
+{
+    public int lengthOfLIS(int[] nums) 
+    {
+        int currLen = -1;
+        int[] dp = new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+        {
+            if (currLen == -1 || nums[i] > dp[currLen])
+            {
+                currLen++;
+                dp[currLen] = nums[i];
+            }
+            else
+            {   // binary search
+                int left = 0, right = currLen, mid = 0;
+                while (left < right)
+                {
+                    mid = left + ((right-left)>>1);
+                    if (nums[i] == dp[mid]) break;
+                    else if(nums[i] > dp[mid]) left = mid+1;
+                    else right = mid;
+                }
+                dp[left] = Math.min(dp[left], nums[i]);
+            }
+        }
+        return currLen+1;
     }
 }
