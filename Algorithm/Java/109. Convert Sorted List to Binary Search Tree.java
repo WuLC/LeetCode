6e31421f462bdf0783d2740f270d3a4b1c93408f
WuLC <liangchaowu5@gmail.com>
2017-05-27 11:38:11 +0800

    Recursive, O(1) space

--- Algorithm/Java/109. Convert Sorted List to Binary Search Tree.java
+++ Algorithm/Java/109. Convert Sorted List to Binary Search Tree.java
@@ -2,7 +2,7 @@
 * @Author: WuLC
 * @Date:   2017-05-27 11:21:08
 * @Last Modified by:   WuLC
-* @Last Modified time: 2017-05-27 11:21:26
+* @Last Modified time: 2017-05-27 11:37:33
 * @Email: liangchaowu5@gmail.com
 */
 
@@ -49,4 +49,56 @@ public class Solution
         root.right = dfs(vals, mid + 1, end);
         return root;
     }
+}
+
+// recursive, O(1) space 
+public class Solution 
+{
+    public TreeNode sortedListToBST(ListNode head) 
+    {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null)
+        {
+            count ++;
+            curr = curr.next;
+        }
+        
+        curr = head;
+        for(int i = 0; i < (count>>1)-1; i++) curr = curr.next;
+        
+        TreeNode root = new TreeNode(curr.next.val);
+        root.right = sortedListToBST(curr.next.next);
+        curr.next = null;
+        root.left = sortedListToBST(head);
+        return root;
+    }
+}
+
+// referer O(1) space solution, the highlight is to traverse the linked once to find the median node
+public class Solution 
+{
+	public TreeNode sortedListToBST(ListNode head) 
+	{
+	    if(head==null) return null;
+	    return toBST(head,null);
+	}
+	public TreeNode toBST(ListNode head, ListNode tail)
+	{
+	    ListNode slow = head;
+	    ListNode fast = head;
+	    if(head==tail) return null;
+	    while(fast!=tail&&fast.next!=tail)
+	    {
+	        fast = fast.next.next;
+	        slow = slow.next;
+	    }
+	    TreeNode thead = new TreeNode(slow.val);
+	    thead.left = toBST(head,slow);
+	    thead.right = toBST(slow.next,tail);
+	    return thead;
+	}
 }
