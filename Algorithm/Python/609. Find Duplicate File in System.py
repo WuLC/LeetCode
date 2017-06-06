650994cc5f2f094265e74fd020090985f33c50ab
WuLC <liangchaowu5@gmail.com>
2017-06-06 16:10:34 +0800

    Hash table

--- Algorithm/Python/609. Find Duplicate File in System.py
+++ Algorithm/Python/609. Find Duplicate File in System.py
@@ -2,36 +2,27 @@
 # @Author: WuLC
 # @Date:   2017-06-06 09:32:10
 # @Last modified by:   WuLC
-# @Last Modified time: 2017-06-06 09:32:26
+# @Last Modified time: 2017-06-06 16:10:20
 # @Email: liangchaowu5@gmail.com
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
-
-# recursive
 class Solution(object):
-    def tree2str(self, t):
+    def findDuplicate(self, paths):
         """
-        :type t: TreeNode
-        :rtype: str
+        :type paths: List[str]
+        :rtype: List[List[str]]
         """
-        s = self.helper(t)
-        return s[1:-1]
-        
-    
-    def helper(self, root):
-        if root == None:
-            return '()'
-        left = self.helper(root.left)
-        right = self.helper(root.right)
-        if right == '()':
-            content = str(root.val) if left == '()' else str(root.val) + left
-        else:
-            content = str(root.val) + left + right
-        return '(' + content + ')'
+        mapping = {}
+        for path in paths:
+            tmp = path.split()
+            directory, files = tmp[0], tmp[1:]
+            for f in files:
+                name, content = f.rstrip(')').split('(')
+                mapping.setdefault(content, [])
+                mapping[content].append(directory + '/' + name)
+        result = []
+        for k, v in mapping.items():
+            if len(v) > 1:
+                result.append(v)
+        return result
             
