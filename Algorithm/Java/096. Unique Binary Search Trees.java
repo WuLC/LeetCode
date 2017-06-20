e2e38542e630ea1f80b042cbe5a675055924b1da
WuLC <liangchaowu5@gmail.com>
2017-06-20 12:23:03 +0800

    Dp without recursive

--- Algorithm/Java/096. Unique Binary Search Trees.java
+++ Algorithm/Java/096. Unique Binary Search Trees.java
@@ -2,7 +2,7 @@
 * @Author: WuLC
 * @Date:   2017-06-20 10:26:55
 * @Last Modified by:   WuLC
-* @Last Modified time: 2017-06-20 10:27:27
+* @Last Modified time: 2017-06-20 12:22:52
 * @Email: liangchaowu5@gmail.com
 */
 
@@ -24,5 +24,32 @@ public class Solution
         }
         cache.put(n, count);
         return count;
+    }
+}
+
+
+// dp without recursive
+public class Solution 
+{
+    // dp[i] is the number of unique BST for i nodes
+    private List<Integer> dp = new ArrayList<Integer>(); 
+    public int numTrees(int n) 
+    {
+        if (dp.size() == 0) 
+        {
+            dp.add(0);
+            dp.add(1);
+        }
+        for (int i = dp.size(); i <= n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < i; j++) // number of nodes on the left sub tree
+            {
+                if ( j == 0 || j == i - 1) count += dp.get(i - 1);
+                else count += dp.get(j) * dp.get(i-j-1);
+            }
+            dp.add(count);
+        }
+        return dp.get(n);
     }
 }
