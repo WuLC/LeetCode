949f3b79e3f53248f876a3c70a48eb365070c7c5
WuLC <liangchaowu5@gmail.com>
2017-07-05 10:45:52 +0800

    1162261467 = 3^^19 since 3^^20 is larger than the largest integer

--- Algorithm/Java/326. Power of Three.java
+++ Algorithm/Java/326. Power of Three.java
@@ -2,12 +2,13 @@
 * @Author: WuLC
 * @Date:   2017-07-05 10:41:26
 * @Last Modified by:   WuLC
-* @Last Modified time: 2017-07-05 10:42:23
+* @Last Modified time: 2017-07-05 10:45:43
 * @Email: liangchaowu5@gmail.com
 */
 
 
 // 1162261467 = 3^19 since 3^20 is larger than the largest integer
+
 public class Solution 
 {
     public boolean isPowerOfThree(int n) 
