bd9e6751308aa67b721fc5551c0091e6a9e6f26b
WuLC <liangchaowu5@gmail.com>
2017-07-05 10:44:32 +0800

    Bit manipulation, lowbit

--- Algorithm/Java/231. Power of Two.java
+++ Algorithm/Java/231. Power of Two.java
@@ -2,7 +2,7 @@
 * @Author: WuLC
 * @Date:   2017-07-05 10:30:05
 * @Last Modified by:   WuLC
-* @Last Modified time: 2017-07-05 10:30:51
+* @Last Modified time: 2017-07-05 10:44:17
 * @Email: liangchaowu5@gmail.com
 */
 
@@ -13,7 +13,6 @@ public class Solution
 {
     public boolean isPowerOfTwo(int n)
     {
-        if (n <= 0) return false;
-        else return  (((n & (-n)) ^ n) == 0);
+        return  n >0 && ((n & (-n)) ^ n) == 0;
     }
 }
---
+++ Algorithm/Java/326. Power of Three.java
@@ -0,0 +1,17 @@
+/*
+* @Author: WuLC
+* @Date:   2017-07-05 10:41:26
+* @Last Modified by:   WuLC
+* @Last Modified time: 2017-07-05 10:42:23
+* @Email: liangchaowu5@gmail.com
+*/
+
+
+// 1162261467 = 3^19 since 3^20 is larger than the largest integer
+public class Solution 
+{
+    public boolean isPowerOfThree(int n) 
+    {
+        return n > 0 && (1162261467 % n == 0);
+    }
+}
