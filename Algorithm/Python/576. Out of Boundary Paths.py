dbe8c2906ea0b89a56e03a4010fe418c44aa2642
WuLC <liangchaowu5@gmail.com>
2017-07-22 15:03:49 +0800

    Dynamic programming, referer: https://leetcode.com/problems/out-of-boundary-paths/#/solution

--- Algorithm/Python/576. Out of Boundary Paths.py
+++ Algorithm/Python/576. Out of Boundary Paths.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author: LC
 # @Date:   2017-07-21 23:54:13
-# @Last modified by:   LC
-# @Last Modified time: 2017-07-21 23:54:51
+# @Last modified by:   WuLC
+# @Last Modified time: 2017-07-22 15:02:57
 # @Email: liangchaowu5@gmail.com
 
-# simple recursive, TLE
+# recursive, TLE
+# time complexity: O(4^n)
 class Solution(object):
     def findPaths(self, m, n, N, i, j):
         """
@@ -25,4 +26,38 @@ class Solution(object):
         for d in dirs:
             count += self.findPaths(m, n, N-1, i+d[0], j+d[1])
         return count
-            
+
+
+# dp, AC
+# time complexity: O(m * n * N)
+# referer: https://leetcode.com/problems/out-of-boundary-paths/#/solution
+class Solution(object):
+    def findPaths(self, m, n, N, i, j):
+        """
+        :type m: int
+        :type n: int
+        :type N: int
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        dp = [[0 for u in xrange(n)] for v in xrange(m)]
+        dp[i][j] = 1
+        M = 1000000007
+        result = 0
+        for _ in xrange(N):
+            tmp = [[0 for u in xrange(n)] for v in xrange(m)]
+            for i in xrange(m):
+                for j in xrange(n):
+                    if i == 0: result += dp[i][j]
+                    if i == m-1: result += dp[i][j]
+                    if j == 0: result += dp[i][j]
+                    if j == n-1: result += dp[i][j]
+                    # update dp for next step    
+                    if i > 0: tmp[i][j] += dp[i-1][j]
+                    if i < m-1: tmp[i][j] += dp[i+1][j]
+                    if j > 0: tmp[i][j] += dp[i][j-1]
+                    if j < n-1: tmp[i][j] += dp[i][j+1]
+                    tmp[i][j] %= M
+            dp = tmp
+        return result
