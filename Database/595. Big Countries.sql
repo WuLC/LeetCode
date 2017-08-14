c456cc011de788ae8ef9c502ebcdc6b74bef48b0
WuLC <liangchaowu5@gmail.com>
2017-08-14 17:39:55 +0800

    Easy

---
+++ Database/595. Big Countries.sql
@@ -0,0 +1,9 @@
+/*
+* @Author: WuLC
+* @Date:   2017-08-14 17:39:10
+* @Last Modified by:   WuLC
+* @Last Modified time: 2017-08-14 17:39:48
+*/
+
+
+SELECT name, population, area FROM World WHERE area > 3000000 OR population > 25000000;
