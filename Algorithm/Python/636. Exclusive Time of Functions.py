72c9e5ae5d4d1a4008ae5f3c507c48d28870fb07
WuLC <liangchaowu5@gmail.com>
2017-08-15 16:33:58 +0800

    More efficient stack, any start means the end of the previous function

--- Algorithm/Python/636. Exclusive Time of Functions.py
+++ Algorithm/Python/636. Exclusive Time of Functions.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2017-08-15 09:49:48
 # @Last Modified by:   WuLC
-# @Last Modified time: 2017-08-15 09:58:49
+# @Last Modified time: 2017-08-15 16:33:43
 
 
 # stack with penalty, penalty is to increment the start time of the functions that are still in the stack
@@ -32,3 +32,29 @@ class Solution(object):
                 
                 
 # more efficient stack
+# any start means the end of the previous function
+
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        """
+        :type n: int
+        :type logs: List[str]
+        :rtype: List[int]
+        """
+        stack  = []
+        result = [0 for _ in xrange(n)]
+        prev = 0
+        for log in logs:
+            id, flag, time = log.split(':')
+            id, time = int(id), int(time)
+            if flag == 'start':
+                if len(stack) > 0:
+                    result[stack[-1]] += time - prev
+                stack.append(id)
+                prev = time
+            else:
+                result[stack.pop()] += time - prev + 1
+                prev = time + 1
+        return result
+                    
+                    
