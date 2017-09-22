91b58f477224a46b55046da021540852a69583af
WuLC <liangchaowu5@gmail.com>
2017-09-22 13:13:06 +0800

    Trie, the most important part is to search in the trie with at most one change

--- Algorithm/Python/676. Implement Magic Dictionary.py
+++ Algorithm/Python/676. Implement Magic Dictionary.py
@@ -2,7 +2,7 @@
 # @Author: WuLC
 # @Date:   2017-09-16 22:07:15
 # @Last Modified by:   WuLC
-# @Last Modified time: 2017-09-16 22:14:53
+# @Last Modified time: 2017-09-22 13:13:03
 
 # hashmap, compare words with same length
 class MagicDictionary(object):
@@ -36,4 +36,60 @@ class MagicDictionary(object):
         return False
 
 
-# 
+# Trie
+# the most important part is to search in the trie with at most one change
+class TrieNode():
+    def __init__(self):
+        self.next = [None] * 26
+        self.isWord = False
+        
+class MagicDictionary(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+
+    def buildDict(self, dict):
+        """
+        Build a dictionary through a list of words
+        :type dict: List[str]
+        :rtype: void
+        """
+        for word in dict:
+            curr = self.root
+            for char in word:
+                idx = ord(char) - ord('a')
+                if curr.next[idx] == None:
+                    curr.next[idx] = TrieNode()
+                curr = curr.next[idx]
+            curr.isWord = True
+        
+            
+                
+    def search(self, word):
+        """
+        Returns if there is any word in the trie that equals to the given word after modifying exactly one character
+        :type word: str
+        :rtype: bool
+        """
+        def helper(root, word, idx, count):
+            if count > 1:
+                return False
+            root_idx = ord(word[idx]) - ord('a')
+            if len(word) - 1 == idx:
+                if count == 1:
+                    return root.next[root_idx] and root.next[root_idx].isWord
+                else: # count  = 0
+                    return any([root.next[i] and root.next[i].isWord for i in xrange(26) if i != root_idx])               
+            for i in xrange(26):
+                if root.next[i]:
+                    if (i == root_idx and helper(root.next[i], word, idx + 1, count)) or \
+                       (i != root_idx and helper(root.next[i], word, idx + 1, count + 1)):
+                        return True
+            return False
+        
+        return helper(self.root, word, 0, 0)
+                   
