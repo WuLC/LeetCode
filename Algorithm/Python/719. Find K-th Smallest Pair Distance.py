29b5cb7ae508990596d136956c48800631c88b50
WuLC <liangchaowu5@gmail.com>
2017-11-19 20:52:11 +0800

    binary search, still get TLE

--- Algorithm/Python/719. Find K-th Smallest Pair Distance.py
+++ Algorithm/Python/719. Find K-th Smallest Pair Distance.py
@@ -26,3 +26,35 @@ class Solution(object):
                 return num
             count -= 1 
 
+
+# binary search, still TLE
+class Solution(object):
+    def smallestDistancePair(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        min_dist, max_dist = nums[-1] - nums[0], nums[-1] - nums[0]
+        for i in xrange(1, len(nums)):
+            min_dist = min(min_dist, nums[i] - nums[i-1])
+        left, right = min_dist, max_dist
+        while left < right:
+            mid = left + ((right-left)>>1);
+            if self.countPairs(nums, mid) < k:
+                left = mid + 1
+            else:
+                right = mid
+        return left 
+    
+    def countPairs(self, nums, dist):
+        """number of pairs whose distance is no more than dist"""
+        count = 0
+        for i in xrange(len(nums)):
+            j = i + 1
+            while j < len(nums) and nums[j] - nums[i] <= dist:
+                count += 1
+                j += 1
+        return count
+        
