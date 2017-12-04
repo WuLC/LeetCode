e1ec6de22f1adfb1ac3ae85254b2f72aafd2fa87
WuLC <liangchaowu5@gmail.com>
2017-12-04 22:58:48 +0800

    method 1, binary search with array

---
+++ Algorithm/Python/729. My Calendar I.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Created on Mon Dec 04 2017 22:56:48
+# Author: WuLC
+# EMail: liangchaowu5@gmail.com
+
+# method 1
+# binary search with array
+# time complexity: bianry search O(logn), insertion O(n)
+class MyCalendar(object):
+
+    def __init__(self):
+        self.record = []
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        if len(self.record) == 0:
+            self.record.append((start, end))
+            return True
+        left, right = 0, len(self.record) - 1
+        while left < right:
+            mid = left + ((right-left)>>1)
+            if self.record[mid][0] >= end:
+                right = mid - 1
+            elif self.record[mid][1] <= start:
+                left = mid + 1
+            else:
+                return False
+            
+        if self.record[left][0]>=end:
+            self.record.insert(left,(start, end))
+            return True
+        elif self.record[left][1]<=start:
+            self.record.insert(left + 1 , (start, end))
+            return True
+        else:
+            return False
+            
