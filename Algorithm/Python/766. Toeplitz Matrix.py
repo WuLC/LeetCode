b39180b013ec6834bea445a4133beaecd248ded7
WuLC <liangchaowu5@gmail.com>
2018-01-23 21:21:59 +0800

    group by indices (row-col)

--- Algorithm/Python/766. Toeplitz Matrix.py
+++ Algorithm/Python/766. Toeplitz Matrix.py
@@ -16,3 +16,20 @@ class Solution(object):
                 if matrix[i][j] != matrix[i-1][j-1]:
                     return False
         return True
+
+# group by indices (row-col)
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        record = {}
+        m, n = len(matrix), len(matrix[0])
+        for i in xrange(m):
+            for j in xrange(n):
+                if i-j not in record:
+                    record[i-j] = matrix[i][j]
+                elif record[i-j] != matrix[i][j]:
+                    return False
+        return True
