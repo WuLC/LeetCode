e74634f0ef116c6894a0ebf5838eba3506e891fd
WuLC <liangchaowu5@gmail.com>
2018-03-27 16:17:48 +0800

    judge if there are only two kinds of length between points,pay attention that same points may appear

--- Algorithm/Python/593. Valid Square.py
+++ Algorithm/Python/593. Valid Square.py
@@ -6,30 +6,23 @@
 # @Email: liangchaowu5@gmail.com
 
 
-# judge if there are only two kinds of length between points firstly
-# then find the neighbors of p1 with the shortest length and judge if the neighboring edge are orthogonal
-from math import sqrt
+# judge if there are only two kinds of length between points
+# pay attention that same points may appear
 class Solution(object):
     def validSquare(self, p1, p2, p3, p4):
-        def distance(a, b):
-            return (a[0]-b[0])**2+(a[1]-b[1])**2
-            
-        record = set()
+        """
+        :type p1: List[int]
+        :type p2: List[int]
+        :type p3: List[int]
+        :type p4: List[int]
+        :rtype: bool
+        """
+        def distance(point1, point2):
+            return (point1[0]-point2[0])**2 + (point1[1]-point2[1])**2
+        all_dist = set()
         points = [p1, p2, p3, p4]
-        for i in xrange(4):
-            for j in xrange(i+1, 4):
-                record.add(distance(points[i], points[j]))
-        if len(record) != 2:
-            return False
-            
-        edge = min(record)
-        p1_neighbors = []
-        for p in points:
-            if distance(p, p1) == edge:
-                p1_neighbors.append(p)
-        p1_vectors = map(lambda p: (p[0]-p1[0], p[1]-p1[1]), p1_neighbors)
-        if len(p1_vectors) != 2: # case [0,0], [1,1], [0,0], [0,0]
-            return False
-        else:
-            return True if reduce(lambda p,q : p[0]*q[0]+p[1]*q[1], p1_vectors) == 0 else False
+        for i in xrange(len(points)):
+            for j in xrange(i+1, len(points)):
+                all_dist.add(distance(points[i], points[j]))
+        return len(all_dist) == 2 and 0 not in all_dist
         
