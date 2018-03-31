bebfb292b03c35b40a96cbacfd2e31902a9dfc64
WuLC <liangchaowu5@gmail.com>
2018-03-31 22:41:28 +0800

    dp, O(1) space, O(n) time

--- Algorithm/Python/152. Maximum Product Subarray.py
+++ Algorithm/Python/152. Maximum Product Subarray.py
@@ -5,18 +5,18 @@
 # @Last Modified time: 2016-07-07 20:25:27
 # @Email: liangchaowu5@gmail.com
 
+# dp, O(1) space, O(n) time
+# record the max number and min number at each position, cause a negative number may turn a min number to a max one
 class Solution(object):
     def maxProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        pmax, nmax, result = nums[0], nums[0], nums[0]
-        for i in xrange(1,len(nums)):
-            if nums[i] < 0:
-                pmax, nmax = nmax, pmax
-            pmax = max(nums[i]*pmax, nums[i])
-            nmax = min(nums[i]*nmax, nums[i])
-            if result < pmax:
-                result = pmax
+        result, curr_max, curr_min = nums[0], nums[0], nums[0]
+        for i in xrange(1, len(nums)):
+            candidate = [nums[i], nums[i]*curr_max, nums[i]*curr_min]
+            curr_max, curr_min = max(candidate), min(candidate)
+            result = max(curr_max, result)
         return result
+        
