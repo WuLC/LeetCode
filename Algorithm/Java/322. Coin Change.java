b504873c964e5d94a8abd36541c3fccb3c09f2e8
WuLC <liangchaowu5@gmail.com>
2018-04-08 18:54:53 +0800

    DP with cache

--- Algorithm/Java/322. Coin Change.java
+++ Algorithm/Java/322. Coin Change.java
@@ -7,30 +7,30 @@
 */
 
 
-# DP, from bottom to top
-public class Solution {
+// DP with cache
+class Solution 
+{
+    private ArrayList<Integer> dp = new ArrayList<Integer>(Arrays.asList(0));
     public int coinChange(int[] coins, int amount) 
     {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp, amount+1);
-        dp[0] = 0;
-        for(int i=1;i<dp.length;i++)
+        Arrays.sort(coins);
+        for(int i=dp.size(); i<=amount; i++)
         {
+            int curr = -1;
             for(int coin:coins)
             {
-                if(i>=coin)
-                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                if (coin > i) break;
+                if (dp.get(i-coin) == -1) continue;
+                curr = curr==-1 ? dp.get(i-coin)+1: Math.min(curr, dp.get(i-coin)+1);
             }
+            dp.add(curr);
         }
-        if (dp[amount] == amount+1)
-            return -1;
-        else
-            return dp[amount];
+        return dp.get(amount);
     }
 }
 
 
-# DP, from top to bottom 
+// DP, from top to bottom 
 public class Solution {
     Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
     public int coinChange(int[] coins, int amount) 
