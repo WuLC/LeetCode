07e05b08ec901b5211290e0a04b42cd9bf697093
WuLC <liangchaowu5@gmail.com>
2018-04-17 10:32:49 +0800

    two pointers, O(n) time; use hashmap to record indices

--- Algorithm/C++/003. Longest Substring Without Repeating Characters.cpp
+++ Algorithm/C++/003. Longest Substring Without Repeating Characters.cpp
@@ -9,17 +9,17 @@
 // use hashmap to record indices
 class Solution 
 {
-public:
-    int lengthOfLongestSubstring(string s)
-    {
-        unordered_map<char, int> idx;
-        int curr = -1, result = 0;
-        for(int i=0; i < s.length(); i++)
+    public:
+        int lengthOfLongestSubstring(string s)
         {
-            if (idx.find(s[i]) != idx.end()) curr = max(curr, idx[s[i]]);
-            result = max(result, i-curr);
-            idx[s[i]] = i;
+            unordered_map<char, int> idx;
+            int curr = -1, result = 0;
+            for(int i=0; i < s.length(); i++)
+            {
+                if (idx.find(s[i]) != idx.end()) curr = max(curr, idx[s[i]]);
+                result = max(result, i-curr);
+                idx[s[i]] = i;
+            }
+            return result;
         }
-        return result;
-    }
 };
