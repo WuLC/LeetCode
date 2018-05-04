421cbf32d82c6a0404582c1924dd18969e419828
WuLC <liangchaowu5@gmail.com>
2018-05-04 18:39:20 +0800

    dp, O(n) space

--- Algorithm/C++/063. Unique Paths II.cpp
+++ Algorithm/C++/063. Unique Paths II.cpp
@@ -31,3 +31,30 @@ class Solution
             return dp[m-1][n-1];
     }
 };
+
+//dp, O(n) space
+class Solution 
+{
+    public:
+        int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+        {
+            if (obstacleGrid.size() == 0) return 0;
+            int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+            int dp[n];
+            for(int i=0; i<m; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                    else
+                    {
+                        if (i==0 && j==0) dp[j] = 1;
+                        else if(i==0) dp[j] = dp[j-1]==0 ? 0 : 1;
+                        else if(j==0) dp[j] = dp[j]==0 ? 0 : 1;
+                        else dp[j] = dp[j] + dp[j-1];   
+                    }
+                }
+            }
+            return dp[n-1];
+        }
+};
