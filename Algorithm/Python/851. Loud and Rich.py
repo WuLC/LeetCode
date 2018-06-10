dc9ed2e3327e555710cff21fdc53d092c27891ac
WuLC <liangchaowu5@gmail.com>
2018-06-10 21:29:36 +0800

    dfs with cache, AC

--- Algorithm/Python/851. Loud and Rich.py
+++ Algorithm/Python/851. Loud and Rich.py
@@ -31,3 +31,33 @@ class Solution(object):
                 r = tmp
         return r
         
+# dfs with cache, AC
+from collections import defaultdict
+class Solution(object):
+    def loudAndRich(self, richer, quiet):
+        """
+        :type richer: List[List[int]]
+        :type quiet: List[int]
+        :rtype: List[int]
+        """
+        self.cache = {}
+        record = defaultdict(list)
+        for x, y in richer:
+            record[y].append(x)
+        result = []
+        for i in xrange(len(quiet)):
+            result.append(self.helper(record, i, quiet))
+        return result
+    
+    def helper(self, record, idx, quiet):
+        """return index of person that is richer than idx and is the least quiet"""
+        if idx not in record:
+            return idx
+        if idx not in self.cache:  
+            r = idx
+            for i in xrange(len(record[idx])):
+                tmp = self.helper(record, record[idx][i], quiet)
+                if quiet[r] > quiet[tmp]:
+                    r = tmp
+            self.cache[idx] = r
+        return self.cache[idx]
