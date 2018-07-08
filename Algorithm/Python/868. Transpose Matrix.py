f6d5bd815506a3fcc936d5767b346b298d53409b
WuLC <liangchaowu5@gmail.com>
2018-07-09 00:04:09 +0800

    simple solution

--- Algorithm/Python/868. Transpose Matrix.py
+++ Algorithm/Python/868. Transpose Matrix.py
@@ -3,19 +3,7 @@
 # Author: WuLC
 # EMail: liangchaowu5@gmail.com
 
-# class Solution(object):
-    def transpose(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        m, n = len(A), len(A[0])
-        result = [[] for _ in xrange(n)]
-        for i in xrange(m):
-            for j in xrange(n):
-                result[j].append(A[i][j])
-        return result
-        
+# simple solution
 class Solution(object):
     def transpose(self, A):
         """
