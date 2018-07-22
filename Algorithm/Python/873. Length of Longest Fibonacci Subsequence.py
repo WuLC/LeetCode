069ca49944f5e382b9ff4d0a85d190083025d34f
WuLC <liangchaowu5@gmail.com>
2018-07-22 20:24:21 +0800

    use any two numbers as starting numbers and construct the sequence

--- Algorithm/Python/873. Length of Longest Fibonacci Subsequence.py
+++ Algorithm/Python/873. Length of Longest Fibonacci Subsequence.py
@@ -3,3 +3,21 @@
 # Author: WuLC
 # EMail: liangchaowu5@gmail.com
 
+# use any two numbers as starting numbers and construct the sequence
+class Solution(object):
+    def lenLongestFibSubseq(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        nums = set(A)
+        result = 0
+        for i in xrange(len(A)):         
+            for j in xrange(i+1, len(A)):
+                tmp = 2   
+                a, b = A[i], A[j]
+                while a+b in nums:
+                    a, b = b, a+b
+                    tmp += 1
+                result = max(result, tmp)
+        return result if result > 2 else 0
