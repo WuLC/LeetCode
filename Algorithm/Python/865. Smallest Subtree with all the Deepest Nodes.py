0ad38be82a30b709100f055355dfee14568d23aa
WuLC <liangchaowu5@gmail.com>
2018-07-30 23:27:09 +0800

    recursive

---
+++ Algorithm/Python/865. Smallest Subtree with all the Deepest Nodes.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Created on Mon Jul 30 2018 23:26:44
+# Author: WuLC
+# EMail: liangchaowu5@gmail.com
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# recursive
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        left, right = self.cal_depth(root.left), self.cal_depth(root.right)
+        if left == right:
+            return root
+        elif left > right:
+            return self.subtreeWithAllDeepest(root.left)
+        else:
+            return self.subtreeWithAllDeepest(root.right)
+    
+    def cal_depth(self, root):
+        if root == None:
+            return 0
+        return 1 + max(self.cal_depth(root.left), self.cal_depth(root.right))
+        
