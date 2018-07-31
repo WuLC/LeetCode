679dcdd6e0312fc308291a59ebaa32571672d548
WuLC <liangchaowu5@gmail.com>
2018-07-31 09:44:35 +0800

    more efficient recursive

--- Algorithm/Python/865. Smallest Subtree with all the Deepest Nodes.py
+++ Algorithm/Python/865. Smallest Subtree with all the Deepest Nodes.py
@@ -30,4 +30,31 @@ class Solution(object):
         if root == None:
             return 0
         return 1 + max(self.cal_depth(root.left), self.cal_depth(root.right))
-        
+
+
+# more efficient recursive
+# referer: https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/discuss/146808/One-pass
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        left, right = self.cal_depth(root.left), self.cal_depth(root.right)
+        if left == right:
+            return root
+        elif left > right:
+            return self.subtreeWithAllDeepest(root.left)
+        else:
+            return self.subtreeWithAllDeepest(root.right)
+    
+    def cal_depth(self, root):
+        if root == None:
+            return (0, None)
+        left, right = self.cal_depth(root.left), self.cal_depth(root.right)
+        if left[0] == right[0]:
+            return (left[0]+1, root)
+        elif left[0] > right[0]:
+            return (left[0]+1, left[1])
+        else:
+            return (right[0]+1, right[1])
