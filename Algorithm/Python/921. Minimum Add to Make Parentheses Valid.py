562ea7b9c955679b473933fd4def3f7ccbfe9135
WuLC <liangchaowu5@gmail.com>
2018-10-23 20:30:13 +0800

    greedy, think with stack

---
+++ Algorithm/Python/921. Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Created on Tue Oct 23 2018 20:28:25
+# Author: WuLC
+# EMail: liangchaowu5@gmail.com
+
+# greedy, think with stack
+class Solution(object):
+    def minAddToMakeValid(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        left, right, result = 0, 0, 0
+        for c in S:
+          if c == '(':
+            left += 1
+          else:
+            if left == 0:
+              result += 1
+            else:
+              left -= 1
+        result += left
+        return result
