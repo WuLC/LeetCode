4ccbcc5336c18869219210697572b8a81ee74950
WuLC <liangchaowu5@gmail.com>
2018-11-23 21:26:23 +0800

    simple solution

---
+++ Algorithm/C++/944. Delete Columns to Make Sorted.cc
@@ -0,0 +1,22 @@
+/*
+ * Created on Fri Nov 23 2018 20:56:19
+ * Author: WuLC
+ * EMail: liangchaowu5@gmail.com
+ */
+
+class Solution {
+  public:
+    int minDeletionSize(vector<string>& A) {
+      int m = A.size(), n = A[0].size();
+      int count = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+          if (A[j][i] < A[j-1][i]) {
+            count++;
+            break;
+          }
+        }
+      }
+      return count;
+    }
+};
