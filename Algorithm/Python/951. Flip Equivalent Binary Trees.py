afe8c1161911ce665f7c6496445c57b1ab940d43
WuLC <liangchaowu5@gmail.com>
2018-12-02 23:53:25 +0800

    make condition more concrete

--- Algorithm/Python/951. Flip Equivalent Binary Trees.py
+++ Algorithm/Python/951. Flip Equivalent Binary Trees.py
@@ -24,5 +24,5 @@ class Solution(object):
             return False
         else:
             return root1.val == root2.val and\
-                   (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right) or\
-                   self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left))
+                   ((self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)) or\
+                    (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)))
