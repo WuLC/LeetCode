f9b181f9c4c54e453f0b243312cd38cac3689527
WuLC <liangchaowu5@gmail.com>
2018-12-17 09:43:11 +0800

    hashmap, there always exists a loop

--- Algorithm/C++/957. Prison Cells After N Days.cc
+++ Algorithm/C++/957. Prison Cells After N Days.cc
@@ -3,3 +3,40 @@
  * Author: WuLC
  * EMail: liangchaowu5@gmail.com
  */
+
+// hashmap, there always exists a loop 
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+using std::vector;
+using std::unordered_map;
+using std::string;
+
+class Solution {
+ public:
+  vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+    unordered_map<string, int> record;
+    unordered_map<int, vector<int>> reversed_record;
+    vector<int> curr(cells);
+    for (int i = 0; i < N; i++) {
+      std::stringstream ss;
+      for(auto digit:curr) ss << char(digit);
+      string s = ss.str();
+      if (record.count(s) > 0) {
+        int left = (N - record[s]) % (i - record[s]);
+        return reversed_record[record[s] + left];
+      } else {
+        record[s] = i;
+        reversed_record[i] = curr;
+      }
+
+      // iterate
+      vector<int> next(8, 0);
+      for (int j = 1; j < 7; j++)
+        next[j] = curr[j-1] == curr[j+1]? 1: 0;
+      curr = next;
+    }
+    return curr;
+  }
+};
