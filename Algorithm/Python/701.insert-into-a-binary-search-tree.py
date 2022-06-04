624ed6d1ad07218d45dafccb71f9f7f60cd93fed
Liangchao Wu <liangchaowu5@gmail.com>
2022-06-04 22:57:21 +0800

    insert into binary search tree

---
+++ Algorithm/Python/701.insert-into-a-binary-search-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=701 lang=python
+#
+# [701] Insert into a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
+# @lc code=end
+
