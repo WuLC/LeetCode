631a0fdb472adbb0d68ab9b924bfeaf5dd184c44
Liangchao Wu <liangchaowu5@gmail.com>
2022-06-06 10:48:38 +0800

    lowest common ancestor

---
+++ Algorithm/C++/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=236 lang=cpp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left == nullptr) {
+            return right;
+        } else if (right == nullptr) {
+            return left;
+        } else {
+            return root;
+        }
+        
+    }
+};
+// @lc code=end
+
---
+++ Algorithm/Python/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=236 lang=python
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left == None:
+            return right
+        elif right == None:
+            return left
+        else:
+            return root
+        
+# @lc code=end
+
