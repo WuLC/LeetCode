ad3dfb1ea78320a2d16595ed3311e139e9d5ea4a
WuLC <liangchaowu5@gmail.com>
2022-08-28 16:31:11 +0800

    two dimension dp with two array/string

--- Algorithm/C++/1035.uncrossed-lines.cpp
+++ Algorithm/C++/1035.uncrossed-lines.cpp
@@ -5,10 +5,23 @@
  */
 
 // @lc code=start
+#include <vector>
+
 class Solution {
 public:
-    vector<int> gridIllumination(int n, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
-        
+    int maxUncrossedLines(std::vector<int>& nums1, std::vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size(), result = 0;
+        std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i+1][j+1] = std::max(dp[i+1][j], dp[i][j+1]);
+                if (nums1[i] == nums2[j]) {
+                    dp[i+1][j+1] = std::max(dp[i][j]+1, dp[i+1][j+1]);
+                }
+                result = std::max(result, dp[i+1][j+1]);
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
