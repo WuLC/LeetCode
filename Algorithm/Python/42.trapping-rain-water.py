3991f78a540b229c498def41032e68b73ee363c7
WuLC <liangchaowu5@gmail.com>
2022-09-20 20:05:17 +0800

    calc column by column

--- Algorithm/Python/42.trapping-rain-water.py
+++ Algorithm/Python/42.trapping-rain-water.py
@@ -5,19 +5,27 @@
 #
 
 # @lc code=start
+
+# calculate volumn of water in each column, O(n) time
 class Solution(object):
     def trap(self, height):
         """
         :type height: List[int]
         :rtype: int
         """
-        result, stack = 0, []
-        for i, h in enumerate(height):
-            while stack and height[stack[-1]] <= h:
-                bottom = height[stack.pop()]
-                if stack:
-                    result += (min(h, height[stack[-1]]) - bottom) * (i - stack[-1] - 1)
-            stack.append(i)
+        n = len(height)
+        maxRHeight = [0] * (n+1)
+        for i in range(n-1, -1, -1):
+            maxRHeight[i] = max(maxRHeight[i+1], height[i])
+
+        result, curr_max = 0, height[0]
+        for i in range(1, n-1):
+            vol = min(curr_max, maxRHeight[i+1]) - height[i]
+            if vol > 0:
+                result += vol
+            curr_max = max(curr_max, height[i])
         return result
 
+        
 # @lc code=end
+
