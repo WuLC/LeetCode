318ba009c704840084ca840ae5cc35a99c6b3401
WuLC <liangchaowu5@gmail.com>
2024-07-01 11:19:49 +0800

    compare dfs with/without cache

--- Algorithm/Python/1000+/1301. Number of Paths with Max Score.py
+++ Algorithm/Python/1000+/1301. Number of Paths with Max Score.py
@@ -1,3 +1,32 @@
+# naive dfs, TLE
+from collections import defaultdict
+
+class Solution(object):
+    def pathsWithMaxScore(self, board):
+        """
+        :type board: List[str]
+        :rtype: List[int]
+        """
+        m, n = len(board), len(board[0])
+        candidates = defaultdict(int)
+        result = [0, 0]
+        def dfs(i, j, _sum): 
+            if board[i][j] == 'E':
+                candidates[_sum] += 1
+                return
+            curr_num = int(board[i][j]) if board[i][j].isdigit() else 0
+            for d in ((1, 0), (0, 1), (1, 1)):
+                ni, nj = i-d[0], j-d[1]
+                if ni >= 0 and nj >= 0 and board[ni][nj] != 'X':
+                    dfs(ni, nj, _sum+curr_num)
+        
+        dfs(m-1, n-1, 0)
+        for k, v in candidates.items():
+            if result[0] <= k:
+                result[0], result[1] = k, v
+        return result
+
+# dfs with cache to avoid TLE
 from collections import defaultdict
 
 class Solution(object):
@@ -10,6 +39,9 @@ class Solution(object):
         memo = defaultdict(list)
 
         def dfs(i, j): 
+            """
+            return: [can reach E from (i, j), max_sum, number of such paths]
+            """
             if board[i][j] == 'E':
                 return [1, 0, 1]
             elif board[i][j] == 'X':
