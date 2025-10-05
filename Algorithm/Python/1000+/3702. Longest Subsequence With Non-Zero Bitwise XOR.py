14c19852092d381f3fe232d5e89f1afb941b5eaf
WuLC <liangchaowu5@gmail.com>
2025-10-05 17:07:55 +0800

    naive dp will lead to TLE

---
+++ Algorithm/Python/1000+/3702. Longest Subsequence With Non-Zero Bitwise XOR.py
@@ -0,0 +1,49 @@
+
+
+# TLE dp solution
+# each idx has two status with xor operation ends with it,(1) result is 0 (2) result is not 0
+# for each status, use a dp array to store the length of the longest subsequence
+# for the non-zero reult, we also need to store the result the longest length
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        result, n = 0, len(nums)
+        zero_len, non_zero_len, non_zero_res = [0]*n, [0]*n, [[] for _ in range(n)]
+        
+        for i in range(n):
+            # init first element
+            if i == 0:
+                if nums[i] > 0:
+                    non_zero_len[i] = 1
+                    non_zero_res[i].append(nums[i])
+                else:
+                    zero_len[i] = 1
+            else:
+                for j in range(i-1, -1, -1):
+                    # non-zero iteration
+                    for _num in non_zero_res[j]:
+                        _len, _res = non_zero_len[j]+1, _num^nums[i]
+                        if _res == 0:
+                            zero_len[i] = max(zero_len[i], _len)
+                        else:
+                            if _len > non_zero_len[i]:
+                                non_zero_len[i] = _len
+                                non_zero_res[i].clear()
+                                non_zero_res[i].append(_res)
+                            elif _len == non_zero_len[i]:
+                                non_zero_res[i].append(_res)
+                    
+                    # zero iteration
+                    _len = zero_len[j]+1
+                    if nums[i] > 0:
+                        if _len > non_zero_len[i]:
+                            non_zero_len[i] = _len
+                            non_zero_res[i].clear()
+                            non_zero_res[i].append(nums[i])
+                        elif _len == non_zero_len[i]:
+                            non_zero_res[i].append(nums[i])
+                    else:
+                        zero_len[i] = max(zero_len[i], zero_len[j]+1)
+                
+            result = max(result, non_zero_len[i])
+        return result
+                    
